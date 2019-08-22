6c34ca6d9e886c4ff2a9d9e8d56d97c7ca371330
Jackson Tubbs <decknot@Jacksons-MacBook-Pro.local>
2019-08-22 11:58:44 -0600

    Initial Commit

---
+++ App/RedditAPIRealOBJ-C/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RedditAPIRealOBJ-C
+//
+//  Created by Jackson Tubbs on 8/22/19.
+//  Copyright © 2019 Jax Tubbs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ App/RedditAPIRealOBJ-C/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RedditAPIRealOBJ-C
+//
+//  Created by Jackson Tubbs on 8/22/19.
+//  Copyright © 2019 Jax Tubbs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
